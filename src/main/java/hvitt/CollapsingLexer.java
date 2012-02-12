96f69d7e329aa2d8b5bbf0a93f10dca1051086b5
jawher <jawher.moussa@gmail.com>
2012-02-12 18:21:11 +0100

    Added a new lexer implementation, which wraps an existing lexer and performs NEWLINE tokens collapsing (merging multiple successive NEWLINE tokens into one), and optionally trim the input, i.e. ignore all NEWLINE tokens at the beginning and end of the input.

---
+++ src/main/java/hvitt/CollapsingLexer.java
@@ -0,0 +1,48 @@
+package hvitt;
+
+public class CollapsingLexer implements Lexer {
+    private final Lexer lexer;
+    private final LexerConfig cfg;
+    private boolean first = true;
+    private final boolean trim;
+    private Token currentToken = null;
+
+    public CollapsingLexer(Lexer lexer, LexerConfig cfg, boolean trim) {
+        this.lexer = lexer;
+        this.cfg = cfg;
+        this.trim = trim;
+    }
+
+    public Token peek() {
+        if (currentToken == null) {
+            currentToken = pop();
+        }
+        return currentToken;
+    }
+
+    public Token pop() throws UnrecognizedInput {
+        if (currentToken != null) {
+            Token res = currentToken;
+            currentToken = null;
+            return res;
+        }
+        Token tk = null;
+        while (cfg.newLineKey.equals(lexer.peek().key)) {
+            tk = lexer.pop();
+        }
+        if (tk != null && (!trim || (!first && !cfg.eofKey.equals(lexer.peek().key)))) {
+            first = false;
+            return tk;
+        } else {
+            return lexer.pop();
+        }
+    }
+
+    public String getRawLine() {
+        return lexer.getRawLine();
+    }
+
+    public String getCurrentColIndentString() {
+        return lexer.getCurrentColIndentString();
+    }
+}
